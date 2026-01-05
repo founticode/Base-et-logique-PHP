1558830797278b5a2a99433ae1df02b717bfff9f
founticode <founticode@gmail.com>
2026-01-05 14:59:27 +0000

    PHP files

---
+++ Chapitre 1 – Déclarer des variables et types dynamiques/variables.php
@@ -0,0 +1,20 @@
+<?php 
+$fullName = "ismail founti";
+$age = 22;
+$prix = 19.99;
+$estConnecte = true;
+
+echo "Nom : " . $fullName . "<br>";
+echo "Age : " . $age . "<br>";
+echo "Price : " . $prix . " €<br>";
+echo "Connected : " . ($estConnecte ? "Oui" : "Non") . "<br>";
+
+var_dump($fullName) . "<br>";
+
+$nombre = "10";
+$nombreInt = (int)$nombre;
+echo "<br>" . "Conversion : " . $nombreInt;
+
+?>
+
+
